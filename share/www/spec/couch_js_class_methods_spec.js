4369f0fe59b2e7bc77ed6dc6c39c637b164e95d7
Lena Herrmann <lena@zeromail.org>
2010-03-17 18:13:55 +0100

    Start of login specs

--- share/www/spec/couch_js_class_methods_spec.js
+++ share/www/spec/couch_js_class_methods_spec.js
@@ -3,60 +3,115 @@
 describe 'CouchDB class'
   before_each
     db = new CouchDB("spec_db", {"X-Couch-Full-Commit":"false"});
-    db.deleteDb();
     db.createDb();
   end
   
-  after
+  after_each
     db.deleteDb();
   end
   
-  describe '.maybeThrowError'
-    it 'should throw an error when the request has status 404'
-      var req = CouchDB.request("GET", "/nonexisting_db");
-      -{CouchDB.maybeThrowError(req)}.should.throw_error 
+  describe '.login'
+    it 'should do sth'
+      // var userDoc = CouchDB.prepareUserDoc({
+      //   name: "Gaius Baltar"
+      // }, "secret");
+      // db.save(userDoc);
+      // CouchDB.login("Gaius Baltar", "secret");
     end
-    
-    it 'should throw an error when the request has status 412'
-      var req = CouchDB.request("PUT", "/spec_db");
-      -{CouchDB.maybeThrowError(req)}.should.throw_error
-    end
-    
-    it 'should throw an error when the request has status 405'
-      var req = CouchDB.request("DELETE", "/_utils");
-      -{CouchDB.maybeThrowError(req)}.should.throw_error
-    end
-    
-    it 'should throw the responseText of the request'
-      var req = CouchDB.request("GET", "/nonexisting_db");
-      try {
-        CouchDB.maybeThrowError(req)
-      } catch(e) {
-        e.error.should.eql JSON.parse(req.responseText).error
-        e.reason.should.eql JSON.parse(req.responseText).reason
-      }
-    end
-    
-    it 'should throw an unknown error when the responseText is invalid json'
-      mock_request().and_return('invalid json...', 'application/json', 404, {})
-      try {
-        CouchDB.maybeThrowError(CouchDB.newXhr())
-      } catch(e) {
-        e.error.should.eql "unknown"
-        e.reason.should.eql "invalid json..."
-      }
-    end
+
   end
 
-  describe '.params'
-    it 'should turn a json object into a http params string'
-      var params = CouchDB.params({'president':'laura', 'cag':'lee'})
-      params.should.eql "president=laura&cag=lee"
-    end
+  describe '.logout'
     
-    it 'should return a blank string when the object is empty'
-      var params = CouchDB.params({})
-      params.should.eql ""
-    end
-  end  
+  end
+
+  describe '.session'
+    
+  end
+
+  describe '.prepareUserDoc'
+    // console.log(CouchDB.prepareUserDoc({'name':'apollo', 'password':'secret'}))
+    // console.log(CouchDB.login('apollo', 'secret'))
+  end
+
+  describe '.allDbs'
+    
+  end
+  
+  describe '.allDesignDocs'
+    
+  end
+  
+  describe '.getVersion'
+    
+  end
+  
+  describe '.replicate'
+    
+  end
+
+  describe '.newXhr'
+    
+  end
+
+  describe '.request'
+    
+  end
+
+  describe '.requestStats'
+    
+  end
+  
+  describe '.newUuids'
+    
+  end
+
+  describe '.maybeThrowError'
+     it 'should throw an error when the request has status 404'
+       var req = CouchDB.request("GET", "/nonexisting_db");
+       -{CouchDB.maybeThrowError(req)}.should.throw_error 
+     end
+     
+     it 'should throw an error when the request has status 412'
+       var req = CouchDB.request("PUT", "/spec_db");
+       -{CouchDB.maybeThrowError(req)}.should.throw_error
+     end
+     
+     it 'should throw an error when the request has status 405'
+       var req = CouchDB.request("DELETE", "/_utils");
+       -{CouchDB.maybeThrowError(req)}.should.throw_error
+     end
+     
+     it 'should throw the responseText of the request'
+       var req = CouchDB.request("GET", "/nonexisting_db");
+       try {
+         CouchDB.maybeThrowError(req)
+       } catch(e) {
+         e.error.should.eql JSON.parse(req.responseText).error
+         e.reason.should.eql JSON.parse(req.responseText).reason
+       }
+     end
+     
+     it 'should throw an unknown error when the responseText is invalid json'
+       mock_request().and_return("invalid json...", "application/json", 404, {})
+       try {
+         CouchDB.maybeThrowError(CouchDB.newXhr())
+       } catch(e) {
+         e.error.should.eql "unknown"
+         e.reason.should.eql "invalid json..."
+       }
+     end
+   end
+  
+   describe '.params'
+     it 'should turn a json object into a http params string'
+       var params = CouchDB.params({"president":"laura", "cag":"lee"})
+       params.should.eql "president=laura&cag=lee"
+     end
+     
+     it 'should return a blank string when the object is empty'
+       var params = CouchDB.params({})
+       params.should.eql ""
+     end
+   end  
 end
